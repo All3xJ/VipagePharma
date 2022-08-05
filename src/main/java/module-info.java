9be419b75550d4b11a34ab65bdaae08212081b7e
paolapitarresi <paola.pitarresi.pp@gmail.com>
2022-08-05 17:38:20 +0200

    schermate aggiornate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,6 @@ module com.vipagepharma {
 
     exports com.vipagepharma.farmacia;
     opens com.vipagepharma.farmacia to javafx.fxml;
-    exports com.vipagepharma.farmacia.gestionePrenotazioni;
-    opens com.vipagepharma.farmacia.gestionePrenotazioni to javafx.fxml;
     exports com.vipagepharma.farmacia.autenticazione.login;
     opens com.vipagepharma.farmacia.autenticazione.login to javafx.fxml;
     exports com.vipagepharma.farmacia.autenticazione.registrazione;
