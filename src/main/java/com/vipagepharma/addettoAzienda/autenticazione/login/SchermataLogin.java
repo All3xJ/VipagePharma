aa5f6b57beb8d50563c7fddc95f178ac57cdc3cd
vitabile <smtp.davidevitabile@gmail.com>
2022-08-06 18:02:56 +0200

    up addetto e corriere

--- src/main/java/com/vipagepharma/addettoAzienda/autenticazione/login/SchermataLogin.java
+++ src/main/java/com/vipagepharma/addettoAzienda/autenticazione/login/SchermataLogin.java
@@ -2,34 +2,50 @@ package com.vipagepharma.addettoAzienda.autenticazione.login;
 
 import com.vipagepharma.addettoAzienda.autenticazione.registrazione.RegistrazioneControl;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SchermataLogin{
+public class SchermataLogin implements Initializable {
 
 	@FXML
-	private TextField username;	// il nome della variabile deve essere lo stesso dell'FXXXXXXID di Scene Builder
+	private TextField username;  // il nome della variabile deve essere lo stesso dell'FXXXXXXID di Scene Builder
 
 	@FXML
-	private TextField password;
+	private PasswordField password;
 
 	@FXML
 	private Button login;
 
+	@Override
+	public void initialize(URL url, ResourceBundle resbound){	// FINALMENTE HO SCOPERTO A CHE SERVE. SERVE PER ESSERE DIOCANEMENTE EVOCATA QUANDO FACCIO setRoot. QUESTO METODO VERRÀ EVOCATO. È UNA SORTA DI MAIN DEL CONTROLLERRR
+		username.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+		password.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	}
+
 	@FXML
-    void onLoginClicked(MouseEvent event) throws IOException{	// sono nella schermata di login. se preme pulsante "login" allora:
+    void premeLogin(MouseEvent event) throws IOException{	// sono nella schermata di login. se preme pulsante "login" allora:
 		System.out.println(username.getText());
 		System.out.println(password.getText());
 		LoginControl logCtrl = new LoginControl(username,password);
 		logCtrl.start();
+    }
+
+	@FXML
+	void premeRegistrati(MouseEvent event) throws IOException{
+		RegistrazioneControl regCtrl = new RegistrazioneControl();
+		regCtrl.start();
 	}
 
 	@FXML
-	void onRegistrazioneClicked(MouseEvent event) throws IOException{
-		RegistrazioneControl regCtrl = new RegistrazioneControl();
-		regCtrl.start();
+	void premeReimpostaPassword(MouseEvent event) throws IOException{
+		// sucami la minchia
+		System.out.println("ciao");
 	}
 }
--- src/main/java/com/vipagepharma/addettoAzienda/autenticazione/registrazione/SchermataRegistrazione.java
+++ src/main/java/com/vipagepharma/addettoAzienda/autenticazione/registrazione/SchermataRegistrazione.java
@@ -1,9 +1,17 @@
 package com.vipagepharma.addettoAzienda.autenticazione.registrazione;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
 public class SchermataRegistrazione{
 
+    @FXML
+    void premeRegistrati(MouseEvent event) throws IOException{
 
     }
+}
 
 
 
