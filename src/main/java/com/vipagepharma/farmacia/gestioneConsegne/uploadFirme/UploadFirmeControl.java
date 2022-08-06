f293fa71bb4f900c01f747f0b5a02092afcaaa19
vitabile <smtp.davidevitabile@gmail.com>
2022-08-06 12:39:28 +0200

    revertone

--- src/main/java/com/vipagepharma/farmacia/gestioneConsegne/uploadFirme/UploadFirmeControl.java
+++
@@ -1,4 +0,0 @@
-package com.vipagepharma.farmacia.gestioneConsegne.uploadFirme;
-
-public class UploadFirmeControl {
-}
--- src/main/java/com/vipagepharma/farmacia/gestioneFarmaci/produzioneFarmaci/ProduzioneFarmaciControl.java
+++
@@ -1,4 +0,0 @@
-package com.vipagepharma.farmacia.gestioneFarmaci.produzioneFarmaci;
-
-public class ProduzioneFarmaciControl {
-}
