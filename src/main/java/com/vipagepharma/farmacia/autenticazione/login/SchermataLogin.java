ffacb34e7a507891d1d233e5ec1e5b6e8c96179d
All3xJ <a.geno@libero.it>
2022-08-06 10:03:12 +0200

    fix vari

--- src/main/java/com/vipagepharma/farmacia/autenticazione/login/SchermataLogin.java
+++ src/main/java/com/vipagepharma/farmacia/autenticazione/login/SchermataLogin.java
@@ -1,14 +1,19 @@
 package com.vipagepharma.farmacia.autenticazione.login;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.vipagepharma.farmacia.App;
 import com.vipagepharma.farmacia.autenticazione.registrazione.RegistrazioneControl;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 
-public class SchermataLogin{
+public class SchermataLogin implements Initializable {
 
 	@FXML
 	private TextField username;  // il nome della variabile deve essere lo stesso dell'FXXXXXXID di Scene Builder
@@ -19,7 +24,11 @@ public class SchermataLogin{
 	@FXML
 	private Button login;
 
-
+	@Override
+	public void initialize(URL url, ResourceBundle resbound){	// FINALMENTE HO SCOPERTO A CHE SERVE. SERVE PER ESSERE DIOCANEMENTE EVOCATA QUANDO FACCIO setRoot. QUESTO METODO VERRÀ EVOCATO. È UNA SORTA DI MAIN DEL CONTROLLERRR
+		username.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+		password.setStyle("-fx-prompt-text-fill: derive(-fx-control-inner-background, -30%);");
+	}
 
 	@FXML
     void onLoginClicked(MouseEvent event) throws IOException{	// sono nella schermata di login. se preme pulsante "login" allora:
