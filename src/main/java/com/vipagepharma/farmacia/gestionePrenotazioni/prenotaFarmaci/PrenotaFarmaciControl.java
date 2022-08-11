ed61a8c8f0096c7d4b038762d38efb274a3b4b0d
vitabile <smtp.davidevitabile@gmail.com>
2022-08-11 12:38:16 +0200

    up Prenota farmaco

--- src/main/java/com/vipagepharma/farmacia/gestionePrenotazioni/prenotaFarmaci/PrenotaFarmaciControl.java
+++ src/main/java/com/vipagepharma/farmacia/gestionePrenotazioni/prenotaFarmaci/PrenotaFarmaciControl.java
@@ -18,6 +18,7 @@ public class PrenotaFarmaciControl {
     private LocalDate data_scadenza_min;
     private String qtyRichiesta;
     private String qtyDisponibile;
+    private String qtyMancante;
     private int flag_scadenza;
     private LocalDate data_consegna;
     private LocalDate new_data_consegna;
@@ -45,31 +46,26 @@ public class PrenotaFarmaciControl {
         ResultSet corrieri = DBMSBoundary.getCorrieri(this.id_farmaco);
         this.id_corriere = this.scegliCorriere(corrieri);
         ResultSet produzione = DBMSBoundary.getProduzione(this.id_farmaco);
-        //DA FARE CHECK DISPONIBILITÁ che torna qtyDisponibile
+        checkDisponibilitaEScegliLotti();
         this.id_farmacia = Utente.getID();
-
-        if(Integer.parseInt(this.qtyDisponibile) > Integer.parseInt(this.qtyRichiesta)){
-            App.setRoot("gestionePrenotazione/prenotaFarmaci/AvvisoPrenotazioneDisponibile");
-        }
-        else{
-            calcProxDisponibilitaEScegliLotti();
-        }
-
-
     }
 
-    public void premutoConferma(){
-        scegliLotti(this.lotti,this.qtyRichiesta);
+    public void premutoConferma() throws SQLException {
+        this.scegliLotti();
         DBMSBoundary.creaPrenotazioneEScarica(this.id_farmacia,this.id_corriere,this.data_consegna,this.idLotti,this.qtyLotti);
     }
 
-    public void scegliLotti(ResultSet lotti,String qty) throws SQLException {
-        int qtyTotale = Integer.parseInt(qty);
+    /* Scegli lotti : scorre i lotti finche ci sono tuple and la qty dei farmaci non ha superato la richiesta; per ogni tupla che
+                          ha la data di disponibilitá < della data di consegna and la data di scandenza > la data accettata di scadenza
+                          aggiunge l'id del lotto alla lista dei lotti e se la qty é abbastanza aggiunge quella che basta, senno tutta
+         */
+    private void scegliLotti() throws SQLException {
+        int qtyTotale = Integer.parseInt(this.qtyRichiesta);
         int qtyLottiTot = 0;
         while(lotti.next() && qtyLottiTot < qtyTotale){
-            if(lotti.getDate(5).toLocalDate().isAfter(this.data_scadenza_min) && lotti.getDate(4).toLocalDate().isAfter(this.data_consegna)){
-                this.idLotti.add(lotti.getInt(1));
-                int qtyLotto = lotti.getInt(3);
+            if(this.lotti.getDate(5).toLocalDate().isAfter(this.data_scadenza_min) && this.lotti.getDate(4).toLocalDate().isBefore(this.data_consegna)){
+                this.idLotti.add(this.lotti.getInt(1));
+                int qtyLotto = this.lotti.getInt(3);
                 qtyLottiTot += qtyLotto;
                 if(qtyLottiTot > qtyTotale){
                     this.qtyLotti.add(qtyTotale - (qtyLottiTot - qtyLotto));
@@ -81,12 +77,53 @@ public class PrenotaFarmaciControl {
         }
     }
 
-    public void calcProxDisponibilitaEScegliLotti(){
-        int qtyMancante = Integer.parseInt(this.qtyDisponibile) - Integer.parseInt(this.qtyRichiesta);
-        //DA FARE
+    private void checkDisponibilitaEScegliLotti() throws SQLException {  //posso calcolare sia la disponibilitá che i lotti con un metodo
+        int qtyTotale = Integer.parseInt(this.qtyRichiesta);
+        int qtyLottiTot = 0;
+        while(lotti.next() && qtyLottiTot < qtyTotale && this.lotti.getDate(4).toLocalDate().isBefore(this.data_consegna)){  //esco dal loop appena la data di disp > data consegna richiesta
+            if(this.lotti.getDate(5).toLocalDate().isAfter(this.data_scadenza_min)){
+                this.idLotti.add(this.lotti.getInt(1));
+                int qtyLotto = this.lotti.getInt(3);
+                qtyLottiTot += qtyLotto;
+                if(qtyLottiTot > qtyTotale){
+                    this.qtyLotti.add(qtyTotale - (qtyLottiTot - qtyLotto));
+                }
+                else{
+                    this.qtyLotti.add(qtyLotto);
+                }
+            }
+        }
+        if(qtyLottiTot <= qtyTotale){  //se dopo il loop non mi sono bastati i farmaci
+            this.qtyDisponibile = String.valueOf(qtyLottiTot);
+            calcProxDisponibilitaEScegliLotti();
+        }
+        else{
+            this.qtyDisponibile = qtyRichiesta;
+        }
     }
 
-    public LocalDate calcDataScadenzaMin(){
+    /* mi servirebbe una tabella con le sole date > tot sarebbe piu semplice qua dovrei comunque riscorrere tutte le tuple */
+
+    private void calcProxDisponibilitaEScegliLotti() throws SQLException {
+        int qtyMancante = Integer.parseInt(this.qtyDisponibile) - Integer.parseInt(this.qtyRichiesta);
+        int qtyLottiTot = 0;
+        while(this.lotti.next() && qtyLottiTot<qtyMancante){
+            this.idLotti.add(this.lotti.getInt(1));
+            int qtyLotto = this.lotti.getInt(3);
+            qtyLottiTot += qtyLotto;
+            if(qtyLottiTot > qtyMancante){
+                this.qtyLotti.add(qtyMancante - (qtyLottiTot - qtyLotto));
+            }
+            else{
+                this.qtyLotti.add(qtyLotto);
+            }
+        }
+        this.qtyMancante = String.valueOf(qtyMancante);
+        this.lotti.previous();
+        this.new_data_consegna = this.lotti.getDate(4).toLocalDate();
+    }
+
+    private LocalDate calcDataScadenzaMin(){
         if(this.flag_scadenza == 0){
             return data_consegna.plusMonths(2);
         }
--- src/main/java/com/vipagepharma/farmacia/gestionePrenotazioni/prenotaFarmaci/SchermataListaFarmaci.java
+++ src/main/java/com/vipagepharma/farmacia/gestionePrenotazioni/prenotaFarmaci/SchermataListaFarmaci.java
@@ -3,6 +3,8 @@ package com.vipagepharma.farmacia.gestionePrenotazioni.prenotaFarmaci;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
+
 public class SchermataListaFarmaci{
 
     private String id_farmaco;
@@ -10,10 +12,8 @@ public class SchermataListaFarmaci{
     public void premeLogout(MouseEvent mouseEvent) {
     }
 
-    public void creaControl(){
+    public void premePrenota() throws IOException {
         PrenotaFarmaciControl prFarCtrl = new PrenotaFarmaciControl();
-        prFarCtrl.start();
+        prFarCtrl.start(this.id_farmaco);
     }
-
-
 }
