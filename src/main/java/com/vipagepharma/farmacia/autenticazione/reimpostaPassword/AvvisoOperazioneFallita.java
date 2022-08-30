619c05616ed43f3521b57305f75cabca5f23d9c3
vitabile <smtp.davidevitabile@gmail.com>
2022-08-30 17:25:58 +0200

    avvisi fixati in autenticazione

--- src/main/java/com/vipagepharma/farmacia/autenticazione/reimpostaPassword/AvvisoOperazioneFallita.java
+++ src/main/java/com/vipagepharma/farmacia/autenticazione/reimpostaPassword/AvvisoOperazioneFallita.java
@@ -2,16 +2,32 @@ package com.vipagepharma.farmacia.autenticazione.reimpostaPassword;
 
 import com.vipagepharma.farmacia.autenticazione.reimpostaPassword.ReimpostaPasswordControl;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class AvvisoOperazioneFallita {
+public class AvvisoOperazioneFallita implements Initializable {
+    @FXML
+    private Text errore_generico;
 
-    private final ReimpostaPasswordControl reimpostaPasswordControl = ReimpostaPasswordControl.repassCtrlRef;
+    @Override
+    public void initialize(URL url, ResourceBundle resbound) {
+        errore_generico.setText(ReimpostaPasswordControl.errore);
+    }
+
 
     @FXML
     public void premeOk(MouseEvent event) throws IOException {
-        reimpostaPasswordControl.premutoOk("autenticazione/reimpostaPassword/SchermataReimpostaPassword");
+        if(ReimpostaPasswordControl.errore.equals("Le password devono coincidere!")){
+            ReimpostaPasswordControl.repassCtrlRef.premutoOk("autenticazione/reimpostaPassword/SchermataNuovaPassword");
+        }
+        else{
+            ReimpostaPasswordControl.repassCtrlRef.premutoOk("autenticazione/reimpostaPassword/SchermataReimpostaPassword");
+        }
+
     }
 }
